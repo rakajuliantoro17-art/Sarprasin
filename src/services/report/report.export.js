4e760e12880b3a84aea30f4d76ce28621e76407e
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-18 20:14:09 +0700

    Add functions to export reports in PDF, Excel, and CSV

---
+++ src/services/report/report.export.js
@@ -0,0 +1,35 @@
+export async function exportPDF(report){
+
+    console.log(
+
+        "Export PDF",
+
+        report
+
+    );
+
+}
+
+export async function exportExcel(report){
+
+    console.log(
+
+        "Export Excel",
+
+        report
+
+    );
+
+}
+
+export async function exportCSV(report){
+
+    console.log(
+
+        "Export CSV",
+
+        report
+
+    );
+
+}

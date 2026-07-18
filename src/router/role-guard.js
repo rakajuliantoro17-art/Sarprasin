9c99092ee4d0c32ddd5d290307252a600ab71faf
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-18 11:26:08 +0700

    Implement role checking in role-guard.js

--- src/router/role-guard.js
+++ src/router/role-guard.js
@@ -1,1 +1,102 @@
+/*
+==================================================
+ROLE GUARD
 
+==================================================
+*/
+
+
+import {
+
+db
+
+}
+
+from "../config/firebase.js";
+
+
+
+import {
+
+doc,
+getDoc
+
+}
+
+from "firebase/firestore";
+
+
+
+
+
+export async function checkRole(
+requiredRole
+){
+
+
+
+const user =
+
+auth.currentUser;
+
+
+
+if(!user)
+
+return false;
+
+
+
+
+
+
+
+const snapshot =
+
+await getDoc(
+
+doc(
+
+db,
+
+"users",
+
+user.uid
+
+)
+
+);
+
+
+
+
+
+
+
+if(!snapshot.exists())
+
+return false;
+
+
+
+
+
+
+
+const data =
+
+snapshot.data();
+
+
+
+
+
+
+return (
+
+data.role===requiredRole
+
+);
+
+
+}

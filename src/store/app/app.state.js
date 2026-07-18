0735c6985f89ed25a1adaf4ccac4297370368b21
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-18 11:43:23 +0700

    Add initial app state configuration

---
+++ src/store/app/app.state.js
@@ -0,0 +1,25 @@
+export const appState = {
+
+
+name:"SARPRASIN",
+
+
+version:"2.0.0",
+
+
+environment:"production",
+
+
+loading:false,
+
+
+online:true,
+
+
+maintenance:false,
+
+
+sidebar:true
+
+
+};

1954459f782fec2b3fdf5c949009f65930ebdc22
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-18 11:25:25 +0700

    Add auth guard to check user authentication status
    Implement authentication guard to check user state.

--- src/router/guards.js
+++ src/router/guards.js
@@ -1,1 +1,62 @@
+/*
+==================================================
+AUTH GUARD
 
+==================================================
+*/
+
+
+import {
+
+auth
+
+}
+
+from "../config/firebase.js";
+
+
+
+import {
+
+onAuthStateChanged
+
+}
+
+from "firebase/auth";
+
+
+
+
+
+
+export function checkAuth(){
+
+
+
+return new Promise(
+
+resolve=>{
+
+
+onAuthStateChanged(
+
+auth,
+
+user=>{
+
+
+resolve(user);
+
+
+}
+
+);
+
+
+}
+
+);
+
+
+
+}

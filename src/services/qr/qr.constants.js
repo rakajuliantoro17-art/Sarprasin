02f2caf3e9cd96071d3d15033b9a4b1ccb3b520a
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-18 20:09:35 +0700

    Create qr.constants.js

---
+++ src/services/qr/qr.constants.js
@@ -0,0 +1,15 @@
+export const QR_PREFIX = "SARPRASIN";
+
+export const QR_VERSION = "2";
+
+export const QR_SEPARATOR = "|";
+
+export const QR_STATUS = {
+
+VALID: "valid",
+
+INVALID: "invalid",
+
+EXPIRED: "expired"
+
+};

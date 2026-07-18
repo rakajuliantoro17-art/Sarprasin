763c88b97d995e7baf9654b4d7eb0610f627d1d9
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-18 11:43:50 +0700

    Add index.js to export various store modules

---
+++ src/store/index.js
@@ -0,0 +1,68 @@
+export {
+
+appStore
+
+} from "./app/app.store.js";
+
+
+export {
+
+authStore
+
+} from "./auth/auth.store.js";
+
+
+export {
+
+userStore
+
+} from "./user/user.store.js";
+
+
+export {
+
+assetStore
+
+} from "./asset/asset.store.js";
+
+
+export {
+
+roomStore
+
+} from "./room/room.store.js";
+
+
+export {
+
+maintenanceStore
+
+} from "./maintenance/maintenance.store.js";
+
+
+export {
+
+reportStore
+
+} from "./report/report.store.js";
+
+
+export {
+
+notificationStore
+
+} from "./notification/notification.store.js";
+
+
+export {
+
+settingsStore
+
+} from "./settings/settings.store.js";
+
+
+export {
+
+cacheStore
+
+} from "./cache/cache.store.js";

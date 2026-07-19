a2883f7452207ef27219ae205747bb35ccc88678
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 19:24:05 +0700

    Implement router service with navigation functions
    Added router service functions for navigation, back, and refresh.

--- src/router/router.service.js
+++ src/router/router.service.js
@@ -1,1 +1,60 @@
+/*
+==================================================
 
+ROUTER SERVICE
+
+==================================================
+*/
+
+
+import {
+
+navigate
+
+}
+
+from "./index.js";
+
+
+
+
+
+export function go(
+
+path
+
+){
+
+
+
+navigate(path);
+
+
+}
+
+
+
+
+
+
+export function back(){
+
+
+
+window.history.back();
+
+
+}
+
+
+
+
+
+export function refresh(){
+
+
+
+location.reload();
+
+
+}

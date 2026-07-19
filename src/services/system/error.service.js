49278114fcd5cf45e9150cbb2e89b32fb6ccb3d5
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 20:13:55 +0700

    Add error handling and user notification
    Implement error handling and logging functionality.

--- src/services/system/error.service.js
+++ src/services/system/error.service.js
@@ -1,1 +1,100 @@
+/*
+==================================================
 
+ERROR SERVICE
+
+==================================================
+*/
+
+
+import {
+
+logError
+
+}
+
+from "./logger.service.js";
+
+
+
+
+export function handleError(
+
+error,
+
+context={}
+
+){
+
+
+
+const errorData={
+
+
+message:
+
+error.message || "Unknown Error",
+
+
+stack:
+
+error.stack,
+
+
+context,
+
+
+time:
+
+new Date()
+
+
+};
+
+
+
+
+
+logError(
+
+errorData
+
+);
+
+
+
+showUserError(
+
+errorData
+
+);
+
+
+
+return errorData;
+
+
+}
+
+
+
+
+
+
+
+function showUserError(
+
+error
+
+){
+
+
+
+console.error(
+
+error
+
+);
+
+
+}

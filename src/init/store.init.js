a1a30cc8fd2f317c317e4556f29def5c083456b5
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 19:18:32 +0700

    Implement store initializer function
    Added a store initializer function to initialize the store and log readiness.

--- src/init/store.init.js
+++ src/init/store.init.js
@@ -1,1 +1,35 @@
+/*
+==================================================
 
+STORE INITIALIZER
+
+==================================================
+*/
+
+
+import {
+
+initializeStore
+
+}
+
+from "../store/index.js";
+
+
+
+
+
+export async function initStore(){
+
+
+initializeStore();
+
+
+console.log(
+
+"Store ready"
+
+);
+
+
+}

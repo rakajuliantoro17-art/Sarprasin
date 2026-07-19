f88264696c8d15b91e04587b36aca4f31d538d1d
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 21:08:03 +0700

    Add application constants for configuration

--- src/services/system/system.constants.js
+++ src/services/system/system.constants.js
@@ -1,1 +1,13 @@
+export const APP_NAME = "SARPRASIN";
 
+export const APP_VERSION = "2.1.0";
+
+export const APP_AUTHOR = "SMAN 1 Sooko Mojokerto";
+
+export const DEFAULT_LANGUAGE = "id";
+
+export const DEFAULT_THEME = "light";
+
+export const CACHE_PREFIX = "sarprasin";
+
+export const STORAGE_PREFIX = "sarprasin";

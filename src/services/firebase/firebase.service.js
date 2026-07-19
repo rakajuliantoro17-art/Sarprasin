17d920ecbf6eefa39b287c9ea9136b6db0d1855b
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 20:43:39 +0700

    Create firebase.service.js to export Firebase modules

---
+++ src/services/firebase/firebase.service.js
@@ -0,0 +1,9 @@
+export * from "./firebase.auth.js";
+
+export * from "./firebase.firestore.js";
+
+export * from "./firebase.storage.js";
+
+export * from "./firebase.analytics.js";
+
+export * from "./firebase.helper.js";

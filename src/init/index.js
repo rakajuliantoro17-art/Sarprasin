b79520c6f132fa8e405e6f6c795a52f3c2b6a636
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 19:17:16 +0700

    Implement application initialization manager
    Added initialization manager for application setup.

--- src/init/index.js
+++ src/init/index.js
@@ -1,1 +1,71 @@
+/*
+==================================================
 
+SARPRASIN v2.0.1
+
+INITIALIZATION MANAGER
+
+==================================================
+*/
+
+
+import {
+
+initConfig
+
+}
+
+from "./config.init.js";
+
+
+import {
+
+initServices
+
+}
+
+from "./service.init.js";
+
+
+import {
+
+initStore
+
+}
+
+from "./store.init.js";
+
+
+import {
+
+initRouter
+
+}
+
+from "./router.init.js";
+
+
+
+
+
+export async function initApplication(){
+
+
+
+await initConfig();
+
+
+
+await initServices();
+
+
+
+await initStore();
+
+
+
+await initRouter();
+
+
+
+}

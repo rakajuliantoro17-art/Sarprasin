2ce08b08c48ca9244b23ef5303979b12354226c3
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 20:14:11 +0700

    Implement logger service with log and logError functions

--- src/services/system/logger.service.js
+++ src/services/system/logger.service.js
@@ -1,1 +1,104 @@
+/*
+==================================================
 
+LOGGER SERVICE
+
+==================================================
+*/
+
+
+const logs=[];
+
+
+
+export function log(
+
+message,
+
+level="INFO"
+
+){
+
+
+
+const data={
+
+
+id:
+
+Date.now(),
+
+
+message,
+
+
+level,
+
+
+time:
+
+new Date()
+
+
+};
+
+
+
+logs.push(data);
+
+
+
+console.log(
+
+`[${level}]`,
+
+message
+
+);
+
+
+
+return data;
+
+
+}
+
+
+
+
+
+
+
+export function logError(
+
+error
+
+){
+
+
+
+return log(
+
+error.message,
+
+"ERROR"
+
+);
+
+
+}
+
+
+
+
+
+
+
+export function getLogs(){
+
+
+
+return logs;
+
+
+}

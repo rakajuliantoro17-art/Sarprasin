d19d95abbbe75d4c35e1af4274b4efb1d95d1a94
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 20:41:00 +0700

    Refactor Firebase connection log to use console.info

--- src/services/firebase/firebase.init.js
+++ src/services/firebase/firebase.init.js
@@ -1,10 +1,18 @@
+/*
+====================================================
+
+Firebase Initializer
+
+====================================================
+*/
+
 import { initializeApp } from "firebase/app";
 
 import { firebaseConfig } from "./firebase.config.js";
 
 export const app = initializeApp(firebaseConfig);
 
-console.log(
-    "🔥 Firebase Connected:",
+console.info(
+    "🔥 Firebase Connected",
     app.options.projectId
 );

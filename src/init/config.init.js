1528d0d571dbc4e2ef7f8094c6647107018ceef4
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 19:17:50 +0700

    Add config initializer with version and environment logging
    Initialize configuration by importing version and environment services, and log the current version and environment.

--- src/init/config.init.js
+++ src/init/config.init.js
@@ -1,1 +1,61 @@
+/*
+==================================================
 
+CONFIG INITIALIZER
+
+==================================================
+*/
+
+
+import {
+
+getVersion
+
+}
+
+from "../services/system/version.service.js";
+
+
+import {
+
+getEnvironment
+
+}
+
+from "../services/system/environment.service.js";
+
+
+
+
+
+export async function initConfig(){
+
+
+
+const version=
+
+getVersion();
+
+
+
+const env=
+
+getEnvironment();
+
+
+
+
+
+console.log(
+
+"SARPRASIN",
+
+version.version,
+
+env
+
+);
+
+
+
+}

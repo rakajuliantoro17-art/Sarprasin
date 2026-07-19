880d6e6ba3873f49bbd2db09578746559e392d36
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 19:33:47 +0700

    Implement user authentication functions

--- src/store/auth.store.js
+++ src/store/auth.store.js
@@ -1,1 +1,48 @@
+import {
 
+getSession
+
+}
+
+from "../services/auth/session.service.js";
+
+
+
+
+let user=null;
+
+
+
+export function initAuth(){
+
+
+user=
+
+getSession();
+
+
+}
+
+
+
+export function getCurrentUser(){
+
+
+return user;
+
+
+}
+
+
+
+export function setUser(
+
+data
+
+){
+
+
+user=data;
+
+
+}

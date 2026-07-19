02132b16f7756fd3e57aafccb75d19d94ab9ccfa
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-19 21:08:55 +0700

    Add default configurations for pagination and profile

---
+++ src/services/system/system.defaults.js
@@ -0,0 +1,15 @@
+export const DEFAULT_PAGINATION = {
+
+    page: 1,
+
+    pageSize: 25
+
+};
+
+export const DEFAULT_PROFILE = {
+
+    photo: "/assets/images/default-avatar.png"
+
+};
+
+export const DEFAULT_DATE_FORMAT = "DD/MM/YYYY";
